25852da4fe45f04ff4af4c446dcbe96cab217b21
wamuyu-gatimu <gatimup21@gmail.com>
2023-07-29 12:44:41 +0300

    some message

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-a=1
+import numpy as np
+
+a=5
 b=5
 c=a+b
-print(c)
+
+if(a>b):
+    print("a is greater than b")
+elif(b>a):
+    print("b is greater than a")
+else:
+    print("C is the greatest")
